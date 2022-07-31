fdaa399a50a7e3758cf8eef42630bdd97334e50a
Dreamice <dreamice13@foxmail.com>
2022-07-31 23:18:40 +0800

    openlayers基础页面 调用点与点距离函数

---
+++ src/views/openlayers/tile.vue
@@ -0,0 +1,52 @@
+<script setup lang='ts'>
+    import 'ol/ol.css'
+    import Map from 'ol/Map'
+    import OSM from 'ol/source/OSM'
+    import TileLayer from 'ol/layer/Tile'
+    import View from 'ol/View'
+    import XYZ from 'ol/source/XYZ'
+    import { ref, onMounted } from 'vue'
+    import { distancePtPt } from '../../utils/distance'
+    
+    const distance = ref(0);
+    distance.value = distancePtPt("112.5007257350843,37.902408272070495,112.50733183000011,37.90022938800007");
+
+    const gaodeTileLayer = new TileLayer({
+		source: new XYZ({
+			url:'http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}'
+		})
+	});
+    
+    const osmTileLayer = new TileLayer({
+        source: new OSM()
+    })
+
+    onMounted(() => {
+        new Map({
+        layers: [gaodeTileLayer, osmTileLayer         
+        ],
+        target: 'map',
+        view: new View({
+            center: [117, 37],
+            zoom: 10,
+            projection: "EPSG:4326",
+        })
+    })
+    })
+    
+
+
+</script>
+
+<template>
+    <span>{{distance}}</span>
+    <div id="map" class="map">
+    </div>
+</template>
+
+<style scoped>
+    .map {
+        width: 100%;
+        height: 100%;
+    }
+</style>
