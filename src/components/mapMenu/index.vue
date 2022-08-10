afd0183b548a0ee9c9150f55ab9cff7a813792fe
Dreamice <dreamice13@foxmail.com>
2022-08-10 23:00:33 +0800

    添加地图菜单组件

---
+++ src/components/mapMenu/index.vue
@@ -0,0 +1,31 @@
+<script setup lang='ts'>
+    import Map from 'ol/Map'
+    // 接收父组件传值
+    const props = defineProps({
+        map: {
+            type: Map
+        }
+    })
+    // 缩小函数
+    const zoomIn = () => {
+        const view = props.map!.getView();
+        const zoom = view.getZoom()!;
+        view.setZoom(zoom - 1);
+    }
+    // 放大函数
+    const zoomOut = () => {
+        const view = (props.map as Map).getView();
+        const zoom: number = view.getZoom()!;
+        view.setZoom(zoom + 1);
+    }
+</script>
+
+<template>
+    <div>
+        <el-button @click="zoomOut">放大</el-button>
+        <el-button @click="zoomIn">缩小</el-button>
+    </div>
+</template>
+
+<style scoped>
+</style>
--- src/views/openlayers/baseMap.vue
+++ src/views/openlayers/baseMap.vue
@@ -7,10 +7,12 @@
     import XYZ from 'ol/source/XYZ'
     import { ref, onMounted } from 'vue'
     import { distancePtPt } from '../../utils/distance'
+    import mapMenu from '../../components/mapMenu/index.vue'
     
     const distance = ref(0);
     distance.value = distancePtPt("112.5007257350843,37.902408272070495,112.50733183000011,37.90022938800007");
 
+    const map = ref();
     const gaodeTileLayer = new TileLayer({
 		source: new XYZ({
 			url:'http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}'
@@ -22,7 +24,7 @@
     })
 
     onMounted(() => {
-        new Map({
+        map.value = new Map({
         layers: [gaodeTileLayer, osmTileLayer         
         ],
         target: 'map',
@@ -39,6 +41,7 @@
 </script>
 
 <template>
+    <mapMenu id="mapMenu" :map="map"></mapMenu>
     <span>{{distance}}</span>
     <div id="map" class="map">
     </div>
@@ -48,5 +51,12 @@
     .map {
         width: 100%;
         height: 100%;
+    }
+
+    #mapMenu {
+        z-index: 1;
+        position: absolute;
+        top: 40px;
+        left: 50px;
     }
 </style>
