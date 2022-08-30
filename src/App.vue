1e9adc9baae32a06cd0fc3dcc2b87dba7d5a737a
Dreamice <dreamice13@foxmail.com>
2022-08-30 22:06:17 +0800

    提交更改内容

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
   <router-view></router-view>
 </template>
 
@@ -17,6 +12,12 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  /* margin-top: 60px; */
+}
+html,body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
 }
 </style>
---
+++ src/views/arcgis/DisplayAMap.vue
@@ -0,0 +1,49 @@
+<script setup lang='ts'>
+    import Map from "@arcgis/core/Map";
+    import WebMap from '@arcgis/core/WebMap'
+    import MapView from '@arcgis/core/views/MapView'
+    import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
+    import { onMounted } from 'vue'
+    onMounted(() => {
+        const featureLayer = new FeatureLayer({
+          url: "http://172.18.193.223:6080/arcgis/rest/services/wgpt/government/FeatureServer/0"
+        });
+
+        const webMap = new WebMap({
+            portalItem: {
+                // id: "aa1d3f80270146208328cf66d022e09c",
+                id: "8dda0e7b5e2d4fafa80132d59122268c"
+            },
+            layers: [featureLayer]
+        })
+
+        const mapView = new MapView({
+            container: "viewDiv",
+            map: webMap,
+            zoom: 10,
+            center: [112.73092308033597, 37.66469795964315]
+        })
+
+        mapView.when(() => {
+            console.log("地图加载成功")
+        })
+    }
+    
+)
+
+
+</script>
+
+<template>
+    <div id="viewDiv">
+
+    </div>
+</template>
+
+<style scoped>
+    #viewDiv {
+        padding: 0;
+        margin: 0;
+        height: 100%;
+    }
+</style>
