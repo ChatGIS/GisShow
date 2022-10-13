1cf405ea856208a7a23e59d2a1e1059c526bf01e
Dreamice <dreamice13@foxmail.com>
2022-10-13 18:16:05 +0800

    添加菜单导航页

--- src/views/system/menu.vue
+++ src/views/system/menu.vue
@@ -1,13 +1,18 @@
 <script setup lang='ts'>
+import getAssetsFile from '../../utils/sys-use'
+
 const menus = [{
     title: '基础地图',
-    imgSrc: 'src/assets/image/map1.png',
-    showRoute: '/olmap'
+    imgSrc: getAssetsFile('map1.png'),
+    showRoute: '/olmap',
+    desc: '基础高德地图展示，包括放大、缩小、定位等功能'
 }, {
     title: '底图换色',
-    imgSrc: 'src/assets/image/map2.png',
-    showRoute: '/colormap'
+    imgSrc: getAssetsFile('map2.png'),
+    showRoute: '/colormap',
+    desc: '切换底图瓦片颜色，快速呈现深蓝色、灰黑色等地图'
 }]
+
 </script>
 
 <template>
@@ -15,9 +20,12 @@ const menus = [{
     <el-row :gutter="20">
         <el-col v-for="(item, index) in menus" :key="item" :span="4">
             <el-card :body-style="{ padding: '0px' }">
-                <img style="width:200px; height:100px" :src="item.imgSrc" class="image" />
                 <div class="info">
                     <router-link :to="item.showRoute">{{item.title}}</router-link>
+                </div>
+                <img style="width:200px; height:100px" :src="item.imgSrc" class="image" />
+                <div class="info desc">
+                    <span>{{item.desc}}</span>
                 </div>
             </el-card>
         </el-col>
@@ -28,7 +36,12 @@ const menus = [{
 img {
     margin: 10px 5px 0;
 }
+
 .info {
     margin: 5px;
+}
+.desc {
+    font-size: smaller;
+    height: 50px;
 }
 </style>
