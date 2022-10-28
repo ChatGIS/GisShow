f583e1c205639922d4f3ae10621fbdacbb65b252
Dreamice <dreamice13@foxmail.com>
2022-10-28 20:07:04 +0800

    vite配置文件格式化

--- vite.config.ts
+++ vite.config.ts
@@ -8,30 +8,30 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gisshow/',
-  plugins: [vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-  }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-  }),
-  eslintPlugin({
-    include: ['src/**/*.js', 'src/**/*.vue', 'src/**/*.ts', 'src/*.js', 'src/*.vue', 'src/*.ts']
-  })
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),  // 路径别名
+    base: '/gisshow/',
+    plugins: [vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+        eslintPlugin({
+            include: ['src/**/*.js', 'src/**/*.vue', 'src/**/*.ts', 'src/*.js', 'src/*.vue', 'src/*.ts']
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),  // 路径别名
+        }
+    },
+    server: {
+        proxy: {
+            '/gaodetile': { // 请求路径关键字
+                target: 'http://webst01.is.autonavi.com/', // 对应自己的接口
+                changeOrigin: true, // 是否允许跨域,在本地会创建一个虚拟服务端，然后发送请求的数据，
+                rewrite: (path) => path.replace(/^\/gaodetile/, '')
+            }
+        }
     }
-  },
-  server: {
-    proxy: {
-      '/gaodetile': { // 请求路径关键字
-        target: 'http://webst01.is.autonavi.com/', // 对应自己的接口
-        changeOrigin: true, // 是否允许跨域,在本地会创建一个虚拟服务端，然后发送请求的数据，
-        rewrite: (path) => path.replace(/^\/gaodetile/, '')
-      }
-    }
-  }
 })
