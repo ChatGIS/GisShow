faa55e49eec88de8aefa2cc56503422e749aa5ca
Dreamice <dreamice13@foxmail.com>
2022-11-16 22:00:42 +0800

    添加element ui icon

--- components.d.ts
+++ components.d.ts
@@ -18,6 +18,7 @@ declare module '@vue/runtime-core' {
     ElDrawer: typeof import('element-plus/es')['ElDrawer']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElOption: typeof import('element-plus/es')['ElOption']
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@arcgis/core/assets/esri/themes/dark/main.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App)
-    .use(router)
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(router)
     .mount('#app')
