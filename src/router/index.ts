d4ae69b14873f036677df2f5852d8ac9caa95f59
Dreamice <dreamice13@foxmail.com>
2023-08-01 23:10:20 +0800

    image

--- src/router/index.ts
+++ src/router/index.ts
@@ -146,7 +146,7 @@ const routes:Array<RouteRecordRaw> = [
     },{
         name: 'StaticImage',
         path: '/static-image',
-        component: () => import('@/views/openlayers/StaticImage1.vue'),
+        component: () => import('@/views/openlayers/StaticImage.vue'),
         meta: {
             keepAlive: false
         }
--- src/views/openlayers/StaticImage2.vue
+++ src/views/openlayers/StaticImage2.vue
@@ -1,9 +1,9 @@
 <!--
  * @Author: Dreamice dreamice13@foxmail.com
- * @Date: 2023-07-19 14:30:14
+ * @Date: 2023-07-19 15:39:13
  * @LastEditors: Dreamice dreamice13@foxmail.com
- * @LastEditTime: 2023-07-28 16:39:04
- * @FilePath: \GisShow\src\views\openlayers\StaticImage.vue
+ * @LastEditTime: 2023-07-20 16:51:09
+ * @FilePath: \GisShow\src\views\openlayers\StaticImage2.vue
  * @Description: 
 -->
 <script setup lang='ts'>
@@ -14,7 +14,8 @@ import { XYZ } from 'ol/source'
 import { onMounted, ref } from 'vue'
 import ImageLayer from 'ol/layer/Image'
 import Static from 'ol/source/ImageStatic'
-import ImgSuidao2 from '@/assets/image/suidao2.jpg'
+import GeoImage from 'ol-ext/source/GeoImage'
+import GeoImageLayera from 'ol-ext/layer/GeoImageLayer'
 
 const map = ref()
 
@@ -22,8 +23,8 @@ onMounted(() => {
     map.value = new Map({
         target: 'mapContainer',
         view: new View({
-            center: [116.397057,39.917315],
-            zoom: 16,
+            center: [116.602922,37.091039],
+            zoom: 10,
             projection: 'EPSG:4326',
         }),
         layers: [
@@ -32,17 +33,22 @@ onMounted(() => {
                     url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}'
                 })
             }),
-            new ImageLayer({
-                source: new Static({
-                    url: ImgSuidao2,
-                    imageExtent: [116.391172, 39.910187, 116.402834, 39.92365]
+            /* new GeoImageLayera({
+                source: new GeoImage({
+                    url: 'https://img.zcool.cn/community/010bbc58229163a84a0e282b16c0c6.jpg?x-oss-process=image/format,webp',
+                    imageCenter: [117.602922,37.071039],
+                    imageScale: [1,1],
+                    imageCrop: [116.602922,36.047337,118.110795,37.091039],
+                    imageMask: [[116.602922,37.091039],[116.602922,36.047337],[118.110795,36.06931],[118.044877,37.231114],[116.602922,37.091039]],
+                    imageRotate: Math.PI/180,
+                    // projection: 'EPSG:4326',
                 })
-            })
+            }) */
         ]
-    }),
-    setTimeout(() => {
+    })
+    /* setTimeout(() => {
         map.value.getView().fit([116.391172, 39.910187, 116.402834, 39.92365])
-    }, 5000)
+    }, 5000) */
 })
 
 </script>
