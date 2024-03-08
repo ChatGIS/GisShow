879885dbcc618df5ca5d1e235a37f08328fce7ef
Dreamice <dreamice13@foxmail.com>
2024-03-08 22:54:36 +0800

    fix menu

--- src/views/system/BaseMenu.vue
+++ src/views/system/BaseMenu.vue
@@ -14,136 +14,136 @@ interface Menu {
 
 // 所有菜单项
 const allMenu = [{
-    title: '基础地图',
-    imgSrc: getAssetsFile('map1.png'),
-    showRoute: '/map-base',
-    desc: '基础高德地图展示，包括放大、缩小、定位等功能'
+  title: '基础地图',
+  imgSrc: getAssetsFile('map1.png'),
+  showRoute: '/map-base',
+  desc: '基础高德地图展示，包括放大、缩小、定位等功能'
+}, {
+  title: '行政区划',
+  imgSrc: getAssetsFile('area_show.png'),
+  showRoute: '/area-show',
+  desc: '行政区划数据：省、市、区县'
+}, {
+  title: 'GIS数据类型',
+  imgSrc: getAssetsFile('menu_data_type.gif'),
+  showRoute: '/data-type',
+  desc: '通过编辑生成各类GIS数据类型，提供编辑、下载功能'
 },{
-    title: '坐标转换',
-    imgSrc: getAssetsFile('map1.png'),
-    showRoute: '/coor-convert',
-    desc: '坐标转换(new)：线要素.geojson'
+  title: '坐标转换',
+  imgSrc: getAssetsFile('map1.png'),
+  showRoute: '/coor-convert',
+  desc: '坐标转换(new)：线要素.geojson'
 },{
-    title: '空间分析',
-    imgSrc: getAssetsFile('map1.png'),
-    showRoute: '/spatial-analysis',
-    desc: '面是否自相交'
+  title: '空间分析',
+  imgSrc: getAssetsFile('map1.png'),
+  showRoute: '/spatial-analysis',
+  desc: '面是否自相交'
 }, {
-    title: '地图纠偏',
-    imgSrc: getAssetsFile('menu_map_rectification.gif'),
-    showRoute: '/map-rectification',
-    desc: '纠偏高德底图；进行坐标转换'
+  title: '地图纠偏',
+  imgSrc: getAssetsFile('menu_map_rectification.gif'),
+  showRoute: '/map-rectification',
+  desc: '纠偏高德底图；进行坐标转换'
 }, {
-    title: '测量工具',
-    imgSrc: getAssetsFile('menu_measure_tool.gif'),
-    showRoute: '/measure-tool',
-    desc: '长度测量、面积测量'
+  title: '测量工具',
+  imgSrc: getAssetsFile('menu_measure_tool.gif'),
+  showRoute: '/measure-tool',
+  desc: '长度测量、面积测量'
 }, {
-    title: '地理编码',
-    imgSrc: getAssetsFile('menu_geo_code.gif'),
-    showRoute: '/geo-code',
-    desc: '地理编码、逆地理编码'
+  title: '地理编码',
+  imgSrc: getAssetsFile('menu_geo_code.gif'),
+  showRoute: '/geo-code',
+  desc: '地理编码、逆地理编码'
 }, {
-    title: '要素样式',
-    imgSrc: getAssetsFile('menu_geo_code.gif'),
-    showRoute: '/feature-style',
-    desc: '点线面基础样式'
+  title: '要素样式',
+  imgSrc: getAssetsFile('menu_geo_code.gif'),
+  showRoute: '/feature-style',
+  desc: '点线面基础样式'
 }, {
-    title: '水波样式',
-    imgSrc: getAssetsFile('menu_geo_code.gif'),
-    showRoute: '/style-ripple',
-    desc: '水波告警样式'
+  title: '水波样式',
+  imgSrc: getAssetsFile('menu_geo_code.gif'),
+  showRoute: '/style-ripple',
+  desc: '水波告警样式'
 }, {
-    title: '双屏联动',
-    imgSrc: getAssetsFile('map_swipe.gif'),
-    showRoute: '/dual-screen-linkage',
-    desc: '双屏联动效果'
+  title: '双屏联动',
+  imgSrc: getAssetsFile('map_swipe.gif'),
+  showRoute: '/dual-screen-linkage',
+  desc: '双屏联动效果'
 }, {
-    title: '地图卷帘',
-    imgSrc: getAssetsFile('map_swipe.gif'),
-    showRoute: '/map-swipe',
-    desc: '基于canvas实现地图卷帘效果'
+  title: '地图卷帘',
+  imgSrc: getAssetsFile('map_swipe.gif'),
+  showRoute: '/map-swipe',
+  desc: '基于canvas实现地图卷帘效果'
 }, {
-    title: '地图卷帘(上下)',
-    imgSrc: getAssetsFile('swipe_vertical.png'),
-    showRoute: '/swipe-vertical',
-    desc: '上下地图对比地铁建设情况'
+  title: '地图卷帘(上下)',
+  imgSrc: getAssetsFile('swipe_vertical.png'),
+  showRoute: '/swipe-vertical',
+  desc: '上下地图对比地铁建设情况'
 }, {
-    title: '底图换色',
-    imgSrc: getAssetsFile('menu_map_color.gif'),
-    showRoute: '/map-color',
-    desc: '切换底图瓦片颜色，快速呈现深蓝色、灰黑色等地图'
+  title: '底图换色',
+  imgSrc: getAssetsFile('menu_map_color.gif'),
+  showRoute: '/map-color',
+  desc: '切换底图瓦片颜色，快速呈现深蓝色、灰黑色等地图'
 }, {
-    title: '在线地图',
-    imgSrc: getAssetsFile('menu_map_online.gif'),
-    showRoute: '/map-online',
-    desc: '常用在线地图'
+  title: '在线地图',
+  imgSrc: getAssetsFile('menu_map_online.gif'),
+  showRoute: '/map-online',
+  desc: '常用在线地图'
 }, {
-    title: '图片配准',
-    imgSrc: getAssetsFile('map1.png'),
-    showRoute: '/static-image',
-    desc: '图片配准'
+  title: '图片配准',
+  imgSrc: getAssetsFile('map1.png'),
+  showRoute: '/static-image',
+  desc: '图片配准'
 }, {
-    title: '捕捉追踪',
-    imgSrc: getAssetsFile('menu_draw_trace.gif'),
-    showRoute: '/draw-trace',
-    desc: '捕捉要素，追踪已有要素，快速绘制'
+  title: '捕捉追踪',
+  imgSrc: getAssetsFile('menu_draw_trace.gif'),
+  showRoute: '/draw-trace',
+  desc: '捕捉要素，追踪已有要素，快速绘制'
 }, {
-    title: 'GIS数据类型',
-    imgSrc: getAssetsFile('menu_data_type.gif'),
-    showRoute: '/data-type',
-    desc: '通过编辑生成各类GIS数据类型，提供编辑、下载功能'
+  title: 'HelloLeaflet',
+  imgSrc: getAssetsFile('map1.png'),
+  showRoute: '/hello-leaflet',
+  desc: '基于Leaflet的地图呈现'
 }, {
-    title: '行政区划',
-    imgSrc: getAssetsFile('menu_data_type.gif'),
-    showRoute: '/area-show',
-    desc: '通过编辑生成各类GIS数据类型，提供编辑、下载功能'
+  title: '飞机航线',
+  imgSrc: getAssetsFile('map1.png'),
+  showRoute: '/antv-l7-flyline',
+  desc: 'antV-L7飞机航线图'
 }, {
-    title: 'HelloLeaflet',
-    imgSrc: getAssetsFile('map1.png'),
-    showRoute: '/hello-leaflet',
-    desc: '基于Leaflet的地图呈现'
+  title: 'HelloCanvas',
+  imgSrc: getAssetsFile('hello_canvas.png'),
+  showRoute: '/hello-canvas',
+  desc: 'Canvas基础'
 }, {
-    title: '飞机航线',
-    imgSrc: getAssetsFile('map1.png'),
-    showRoute: '/antv-l7-flyline',
-    desc: 'antV-L7飞机航线图'
+  title: '楼层选择控件',
+  imgSrc: getAssetsFile('floor_bar.png'),
+  showRoute: '/indoor-map',
+  desc: '楼层选择控件'
 }, {
-    title: 'HelloCanvas',
-    imgSrc: getAssetsFile('hello_canvas.png'),
-    showRoute: '/hello-canvas',
-    desc: 'Canvas基础'
-}, {
-    title: '楼层选择控件',
-    imgSrc: getAssetsFile('floor_bar.png'),
-    showRoute: '/indoor-map',
-    desc: '楼层选择控件'
-}, {
-    title: '计算中心点',
-    imgSrc: getAssetsFile('floor_bar.png'),
-    showRoute: '/calc-center',
-    desc: '计算中心点'
+  title: '计算中心点',
+  imgSrc: getAssetsFile('floor_bar.png'),
+  showRoute: '/calc-center',
+  desc: '计算中心点'
 }]
 
 // 计算属性，拆为二维数组
 const menus = computed(() => {
-    const menuArr: Menu[][] = []
-    allMenu.forEach((item: Menu, index: number) => {
-        const row = Math.floor(index / 4)
-        if (!menuArr[row]) {
-            menuArr[row] = []
-        }
-        menuArr[row].push(item)
-    })
-    return menuArr
+  const menuArr: Menu[][] = []
+  allMenu.forEach((item: Menu, index: number) => {
+    const row = Math.floor(index / 4)
+    if (!menuArr[row]) {
+      menuArr[row] = []
+    }
+    menuArr[row].push(item)
+  })
+  return menuArr
 })
 // 计算数量
 const menuNum = computed(() => {
-    return allMenu.length
+  return allMenu.length
 })
 // 跳转页面
 const toPage = (route: string) => {
-    router.push(route)
+  router.push(route)
 }
 </script>
 
